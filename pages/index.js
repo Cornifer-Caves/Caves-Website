737590532acd67be574544038ae6f69285efbdc1
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-01 20:02:36 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="My name is cornifer_caves. I make bad games badly" />
+    <p> I make games and bad stories <p>
 <iframe height="167" frameborder="0" src="https://itch.io/embed/1630636" width="552"><a href="https://coolio-caves.itch.io/p0ng-i">p0ng = i by Cornifer_Caves</a></iframe>
       </main>
 
