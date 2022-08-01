f320351dacf082f04e8183014313ce446b6f0d73
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-01 20:12:28 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,7 @@ export default function Home() {
       <main>
         <Header title="My name is cornifer_caves. I make bad games badly" />
     <h1>Games<h1>
-    <p>I make games frquently but p0ng is the most recent<p>
 <iframe height="167" frameborder="0" src="https://itch.io/embed/1630636" width="552"><a href="https://coolio-caves.itch.io/p0ng-i">p0ng = i by Cornifer_Caves</a></iframe>
-    <p>This my most recent game however Im always cooking up something<p>
       </main>
 
       <Footer />
