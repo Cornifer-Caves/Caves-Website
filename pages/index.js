a024837058fdc4b09fb6ab406edf7b477a432ea9
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-01 19:39:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,39 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Cornifer has a website?!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-
-<iframe src="https://itch.io/embed/1628145" width="640" height="640" frameborder="0"><a href="https://coolio-caves.itch.io/p0ng">p0ng by Cornifer_Caves</a></iframe>
-
-
-      <Footer />
-    </div>
-  )
-}
+<html>
+<head>
+<title>
+Cornifer's Website
+</title>
+</head>
+<body>
+<p>
+<font face="Comic sans MS" size="7">
+subscribe
+</body>
+<p>
+</font>
+<body>
+<font face="Comic sans MS" size="5">
+to cornifersleeps
+</body>
+</font>
+<body>
+<p>
+<font face="Times New Roman" size="1">
+https://www.youtube.com/channel/UCfidLSYHDDIBVW1zRYNhq5A
+</font>
+<p>
+</body>
+<body>
+<p>
+<img src="https://video-images.vice.com/articles/5dfcdf5174a820009a1b4a41/lede/1576853802436-chicky-nuggies.jpeg?crop=1xw%3A1xh%3Bcenter%2Ccenter&resize=2000%3A*" alt="HTML5 Image" style="width:400px; height:250px;">
+</p>
+</body>
+<body>
+<p>
+<iframe width="853" height="480" src="https://www.youtube.com/embed/x8iLN_WvjqM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</p>
+</body>
+</html>
