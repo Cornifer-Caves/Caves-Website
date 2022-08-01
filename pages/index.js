e13043d895ae44306225c328d90aa1089b45c496
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-01 20:37:44 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,15 @@ export default function Home() {
 
       <main>
         <Header title="Cornifer has a website!?" />
-<blockquote class="imgur-embed-pub" lang="en" data-id="a/eihA0VM" data-context="false" ><a href="//imgur.com/a/eihA0VM"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
         <p className="description">
           yeah apparently I wanted one so here it is
         </p>
+        <p>
+          If you dont know me I make amature games on my itch.io
+        </p>
+        <p>
+          https://coolio-caves.itch.io/
+        </p>        
         <p>
           If you dont know me I make amature games such as
         </p>
