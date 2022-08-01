b815db32f5fff8414f351447f5430069f1775f8e
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-01 19:32:40 +0100

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -2,11 +2,6 @@ describe('sample test', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
-  it('displays the resources text', () => {
-    cy.get('h1')
-    .contains(' ');
-  })
   it('renders the Netlify logo image', () => {
     cy.get('img')
     .should('be.visible')
