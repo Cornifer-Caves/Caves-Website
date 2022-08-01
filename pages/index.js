53beccc921f8d8cd67bc402dee540e26573c22d5
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-01 20:28:18 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Cornifer has a website!?" />
+<blockquote class="imgur-embed-pub" lang="en" data-id="a/eihA0VM" data-context="false" ><a href="//imgur.com/a/eihA0VM"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
         <p className="description">
           yeah apparently I wanted one so here it is
           If you dont know me I make amature games such as
