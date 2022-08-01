2255d0e76ab58cc1d1037797d23e52486a70e118
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-01 20:01:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>coolio-site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="My name is cornifer_caves. I make bad games badly" />
 <iframe height="167" frameborder="0" src="https://itch.io/embed/1630636" width="552"><a href="https://coolio-caves.itch.io/p0ng-i">p0ng = i by Cornifer_Caves</a></iframe>
       </main>
 
