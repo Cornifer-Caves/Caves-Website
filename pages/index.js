76a35b9c19eae1ced2f2dd54b3a69177c92c201a
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-01 20:25:46 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,6 @@ export default function Home() {
         <Header title="Cornifer has a website!?" />
         <p className="description">
           yeah apparently I wanted one so here it is
-        </p>
-        <p className="description">
           If you dont know me I make amature games such as
         </p>
     <iframe width="552" height="167" frameborder="0" src="https://itch.io/embed/1628145"><a href="https://coolio-caves.itch.io/p0ng">p0ng by Cornifer_Caves</a></iframe>
