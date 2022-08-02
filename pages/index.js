9188824bdb99e9a1f470ba41707b07b1b0c7e611
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-02 16:27:47 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,14 @@ export default function Home() {
           I make devlogs sometimes and keep people updated on my projects.
         </p> 
         <p>
+          I have more to say but to see that,
+        </p> 
+          join <a href="https://discord.gg/NV8d5hrGBE">my discord :3</a>
+        <p>
           <img src="https://i.ibb.co/jHjBXB7/spirit.png" alt="A cartoon ghost" />
+        </p>
+          <p>
+          Music by flowerhead I need to make it autoplay
         </p>
         <iframe src="https://drive.google.com/file/d/1vQJFGE2A9KV2E5j3ePiPO3ajgLG533m9/preview" width="320" height="64" allow="autoplay"></iframe>
       </main>
