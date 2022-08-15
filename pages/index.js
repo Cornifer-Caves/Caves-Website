f935f4fe1b7c750e3971359646fdd25a5ab4743b
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-15 22:09:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,26 +19,11 @@ export default function Home() {
         </p>
         <Header title="Who is Cornifer" />
         <p>
-          My name, cornifer, is from the game hollow knight (which i havent yet beat).
-        </p>
-        <p>
-          If you dont know me I make amature games on <a href="https://coolio-caves.itch.io/">my itch.io</a>
-        </p>
-        <p>
-          keep up with my itch because Im always doing jams!
-        </p>
-        <p>
-          I make videos aswell on <a href="https://www.youtube.com/channel/UCdDG5aOkGhZzWIfIP9v3chA">my Youtube</a>
-        </p>
-        <p>
-          I make devlogs sometimes and keep people updated on my projects.
-        </p> 
-        <p>
-          I have more to say but to see that,
-        </p> 
-          join <a href="https://discord.gg/NV8d5hrGBE">my discord :3</a>
-        <p>
-          <img src="https://i.ibb.co/jHjBXB7/spirit.png" alt="A cartoon ghost" />
+          My name is cornifer, I'm an amature game developer
+        <br/>
+          I stream and make videos aswell.
+        <br/>
+          I have a discord that chat that's just chill place to hangout
         </p>
       </main>
       <div class="fixed">
