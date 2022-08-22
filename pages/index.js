d3563ed8c378cd32ec596cbe0d137de003bb0c7d
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-22 06:32:13 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <body>
-     <ul>
-       <li><a href="#https://coolio-caves.netlify.app/">Home</a></li>
-       <li><a href="https://coolio-caves.netlify.app/">News</a></li>
-       <li><a href="https://coolio-caves.netlify.app">Contact</a></li>
-     </ul> 
       <main>
         <Header title="Cornifer has a website!?" />
         <p className="description">
           yeah apparently I wanted one so here it is
         </p>
+     <ul>
+       <li><a href="#https://coolio-caves.netlify.app/">Home</a></li>
+       <li><a href="https://coolio-caves.netlify.app/">News</a></li>
+       <li><a href="https://coolio-caves.netlify.app">Contact</a></li>
+     </ul> 
       </main>
      <Footer />
     </body>
