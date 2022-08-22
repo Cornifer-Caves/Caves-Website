d1a70074b51f4f89b83df30452e8b3eca9404253
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-22 07:37:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,14 @@ export default function Home() {
       <main>
         <Header title="Cornifer has a website!?" />
         <p className="description">
-          yeah apparently I wanted one so here it is
+          I'm an amature game developer who's been using godot for about 6 months
         </p>
+        <p>
+          
      <ul>
-       <li><a href="#https://coolio-caves.netlify.app/">Home</a></li>
-       <li><a href="https://coolio-caves.netlify.app/">News</a></li>
-       <li><a href="https://coolio-caves.netlify.app">Contact</a></li>
+       <li><a href="https://coolio-caves.netlify.app">about</a></li>
+       <li><a href="https://coolio-caves.netlify.app">games</a></li>
+       <li><a href="https://coolio-caves.netlify.app">videos</a></li>
      </ul> 
       </main>
      <Footer />
