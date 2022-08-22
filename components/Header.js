03f67d1a7ec471ac66a7e0d74362712611f6c600
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-22 03:56:31 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,19 @@
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
-       <div class="fixed">
-        <a href="https://discord.gg/NV8d5hrGBE" target="_blank">
-         <img src="/Discord.png" alt="https://discord.gg/NV8d5hrGBE" width="64" height="64"/>
-        </a>
-        <a href="https://twitch.tv/garfieldcatboy" target="_blank">
-         <img src="/Twitch.png" alt="https://twitch.tv/garfieldcatboy" width="64" height="64"/>
-        </a>
-        <a href="https://www.youtube.com/channel/UCdDG5aOkGhZzWIfIP9v3chA" target="_blank">
-         <img src="/Youtube.png" alt="https://www.youtube.com/channel/UCdDG5aOkGhZzWIfIP9v3chA" width="64" height="64"/>
-        </a>
-        <a href="https://coolio-caves.itch.io/" target="_blank">
-         <img src="/itchio.png" alt="https://coolio-caves.itch.io/" width="64"height="64"/>
-        </a>
-
-      </div> 
+  
 }
+<div class="fixed">
+  <a href="https://discord.gg/NV8d5hrGBE" target="_blank">
+   <img src="/Discord.png" alt="https://discord.gg/NV8d5hrGBE" width="64" height="64"/>
+  </a>
+  <a href="https://twitch.tv/garfieldcatboy" target="_blank">
+   <img src="/Twitch.png" alt="https://twitch.tv/garfieldcatboy" width="64" height="64"/>
+  </a>
+  <a href="https://www.youtube.com/channel/UCdDG5aOkGhZzWIfIP9v3chA" target="_blank">
+   <img src="/Youtube.png" alt="https://www.youtube.com/channel/UCdDG5aOkGhZzWIfIP9v3chA" width="64" height="64"/>
+  </a>
+  <a href="https://coolio-caves.itch.io/" target="_blank">
+   <img src="/itchio.png" alt="https://coolio-caves.itch.io/" width="64"height="64"/>
+  </a>
+
+ </div> 
