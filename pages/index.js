a667f53e0a2aab953ae02dabe3516b15c7b5ef59
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-22 07:55:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,10 @@ export default function Home() {
        <li><a href="https://coolio-caves.netlify.app">about</a></li>
        <li><a href="https://coolio-caves.netlify.app">games</a></li>
        <li><a href="https://coolio-caves.netlify.app">videos</a></li>
+       <li><a href="https://www.youtube.com/channel/UCdDG5aOkGhZzWIfIP9v3chA">youtube</a></li>
+       <li><a href="https://www.twitch.tv/garfieldcatboy">twitch</a></li>
+       <li><a href="https://discord.gg/NV8d5hrGBE">discord</a></li>
+       <li><a href="https://coolio-caves.itch.io">itch.io</a></li>
      </ul> 
       </main>
      <Footer />
