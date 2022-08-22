a66d292ee437e03e4adfefdfaca8e805a7bb68d4
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-22 06:20:20 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <body>
+     <ul>
+       <li><a href="#home">Home</a></li>
+       <li><a href="#news">News</a></li>
+       <li><a href="#contact">Contact</a></li>
+       <li style="float:right"><a class="active" href="#about">About</a></li>
+     </ul> 
       <main>
         <Header title="Cornifer has a website!?" />
         <p className="description">
           yeah apparently I wanted one so here it is
         </p>
-
       </main>
     </body>
-    <Footer />
     </div>
   )
 }
