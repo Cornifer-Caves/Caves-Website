7b6f7cd7f3c17f64662465cd08d6c63974c6cbba
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-22 06:05:11 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,32 +11,12 @@ export default function Home() {
       </Head>
     <body>
       <main>
-        <Header title="Who is cornifer?." />
+        <Header title="Cornifer has a website!?" />
         <p className="description">
-          I'm amature game developer who's been using godot for about 6 months
+          yeah apparently I wanted one so here it is
         </p>
-        <p>
-          not only do I make games I make videos and stream!
-        </p>
-        <p>
-          I'm active in the 
-         
-      </main>
-      <div class="fixed">
-        <a href="https://discord.gg/NV8d5hrGBE" target="_blank">
-         <img src="/Discord.png" alt="https://discord.gg/NV8d5hrGBE" width="64" height="64"/>
-        </a>
-        <a href="https://twitch.tv/garfieldcatboy" target="_blank">
-         <img src="/Twitch.png" alt="https://twitch.tv/garfieldcatboy" width="64" height="64"/>
-        </a>
-        <a href="https://www.youtube.com/channel/UCdDG5aOkGhZzWIfIP9v3chA" target="_blank">
-         <img src="/Youtube.png" alt="https://www.youtube.com/channel/UCdDG5aOkGhZzWIfIP9v3chA" width="64" height="64"/>
-        </a>
-        <a href="https://coolio-caves.itch.io/" target="_blank">
-         <img src="/itchio.png" alt="https://coolio-caves.itch.io/" width="64"height="64"/>
-        </a>
 
-      </div> 
+      </main>
     </body>
     </div>
   )
