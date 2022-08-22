5ca84a32f67443c367c7037a91e90a6e8bc066f4
Cornifer-Caves <110419243+Cornifer-Caves@users.noreply.github.com>
2022-08-22 05:21:23 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,24 +9,21 @@ export default function Home() {
         <title>a-coolio-site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-
-    <body>
-      <main>
-        <Header title="Cornifer's Work." />
-        <p className="description">
-          yeah apparently I wanted one so here it is
-        </p>
-        <Header title="Games" />
-        <p>
-          <iframe src="https://itch.io/embed-upload/6366629?color=7b67a6" allowfullscreen="" width="600" height="252" frameborder="0"><a href="https://coolio-caves.itch.io/endless-survival-game">Play Endless survival game on itch.io</a></iframe>
-        </p>
         <ul>
           <li><a href="https://coolio-caves.itch.io/">Home</a></li>
           <li><a href="https://coolio-caves.itch.io/">News</a></li>
           <li><a href="https://coolio-caves.itch.io/">Contact</a></li>
           <li><a href="https://coolio-caves.itch.io/">About</a></li>
         </ul>
+    <body>
+      <main>
+        <Header title="Who is cornifer?." />
+        <p className="description">
+          yeah apparently I wanted one so here it is
+        </p>
+     
+        <p>
+
       </main>
       <div class="fixed">
         <a href="https://discord.gg/NV8d5hrGBE" target="_blank">
